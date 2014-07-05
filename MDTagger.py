0ec374c935233ee27ab5195d1d96a462527e1e79
wyldphyre <craigreynolds@Claire.local>
2014-07-05 13:16:07 +0800

    Basic functionality of extracting filename information now works and is listed as output from the script.

--- MDTagger.py
+++ MDTagger.py
@@ -1,5 +1,20 @@
 import sys
 import os
+import re
+import titlecase
+
+def clean_issue(source):
+    return source.lstrip('0')
+
+def clean_title(source):
+    return titlecase.titlecase(source.replace('.', ' ').lstrip().rstrip())
+
+#def issue_is_valid(issue):
+#    return str.isdigit(issue)
+
+# todo: allow script to run in interactive mode
+# Interactive mode would let the user provide a simple y/n answer
+# as to whether each files extracted info should be saved to file
 
 folder_name = sys.argv[1]
 print folder_name
@@ -15,5 +30,20 @@ directory_list = os.listdir(folder_name)
 for filename in directory_list:
     full_file_path = os.path.join(folder_name, filename)
     if os.path.isfile(full_file_path):
-        print full_file_path
+        print "Processing: %s" % filename
+        # look for the issue number and title
+        match = re.search('\.(\d*)\.\.(.*)\.cbz|cbr', filename)
+        if match:
+            issue = match.group(1)
+            title = match.group(2)
+
+            issue = clean_issue(issue)
+            title = clean_title(title)
+
+            # todo: validate issue is a clean usable number
+
+            print "Found Issue: %s" % issue
+            print "Found Title: %s" % title
+
+        print ""
 
